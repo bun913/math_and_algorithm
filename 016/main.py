27911856fd88269e4d9d79b887b0b588870e75c8
bun913 <imanau03@gmail.com>
2023-05-17 08:46:15 +0900

    feat: add ユークリッドの互助方

--- 016/main.py
+++ 016/main.py
@@ -1,35 +1,26 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+N個の整数の最大公約数を求める
+Nが10**5,でAが10**18なので普通に1から数え上げると間に合わない
+よってユークリッドの互助法を用いて1個のGCDをlogNで求める
+N*logNで求めることができる
 """
+from functools import reduce
 
-def gcd(a: int, b: int) -> int:
-    small = b if a > b else a
-    big = a if a > b else b
-    if small == 0:
-        return big
-    return gcd(big % small, small)
+def my_gcd(l:int, r:int) -> int:
+    a = l
+    b = r
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+        if a == 0:
+            return b
+        if b == 0:
+            return a
 
-n = input()
-l = list(map(int, input().split()))
-# 計算
-ans = gcd(l.pop(), l.pop())
-for i in range(2, len(l)):
-    ans = gcd(ans, l.pop())
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = reduce(my_gcd, A)
 print(ans)
