84832d713f3d8f3883f09c377e4d0508213260b4
bun913 <imanau03@gmail.com>
2023-05-18 06:07:53 +0900

    feat: add 500円の組み合わせ

--- 018/main.py
+++ 018/main.py
@@ -1,30 +1,16 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+Nが200,000もある
+2つを選ぶと10**10となり間に合わない
+Nの時間計算量で求める必要がある
 """
+from collections import Counter
 
-# 与えられるものが100,200,300,400のうちのいずれかだけ
-# そのうち2つを選んで500になるのは
-# a: 100 , 400 と
-# b: 200,300 の組み合わせだけ
-# 100円の数 * 400円の数 + 200円の数 * 300円の数
-n = int(input())
-l = list(map(int, input().split()))
+N = int(input())
+A = list(map(int, input().split()))
+C = Counter(A)
 
-ab = l.count(100) * l.count(400)
-cd = l.count(200) * l.count(300)
-print(ab + cd)
+ans = 0
+for combi in [(100,400), (200,300)]:
+    l, r = combi
+    ans += C[l] * C[r]
+print(ans)
