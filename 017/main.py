7199e46fb24d9c848760cbbe64cd61bb7a424b85
bun913 <imanau03@gmail.com>
2023-05-18 05:49:05 +0900

    feat: add lcm

--- 017/main.py
+++ 017/main.py
@@ -1,44 +1,25 @@
-"""
-Atcoderの問題解く用
+from functools import reduce
 
-1行1列データ
+def my_gcd(l:int, r:int) -> int:
+    a = l
+    b = r
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+        if a == 0:
+            return b
+        if b == 0:
+            return a
 
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
+def my_lcm(l:int, r:int) -> int:
+    g = my_gcd(l, r)
+    lcm = g * (l // g) * (r // g)
+    return lcm
 
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
+N = int(input())
+A = list(map(int, input().split()))
 
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-"""
-
-# N個の最小公倍数を求める問題
-# 最小公倍数を求めるには a * b / aとbの最大公約数
-
-def max_divisor(a: int, b: int) -> int:
-    small= b if a > b else a
-    big = a if a > b else b
-    # ベースケース
-    if small == 0:
-        return big
-    return max_divisor(small, big % small)
-
-def least_common(a:int, b:int) -> int:
-    return (a * b) // max_divisor(a, b)
-
-n = int(input())
-l = list(map(int, input().split()))
-a = l.pop()
-b = l.pop()
-ans = least_common(a,b)
-for i in range(2, n):
-    a = l.pop()
-    ans = least_common(ans, a)
+ans = reduce(my_lcm, A)
 print(ans)
