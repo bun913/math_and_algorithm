d37a071b805e610e5cb5cfa908648d1813fc4333
bun913 <imanau03@gmail.com>
2023-05-20 10:07:39 +0900

    feat: def my combination

--- 021/main.py
+++ 021/main.py
@@ -1,25 +1,14 @@
-"""
-Atcoderの問題解く用
+from math import factorial
+# set max recursin depth 10 ** 9
+import sys
+sys.setrecursionlimit(10 ** 9)
 
-1行1列データ
+def my_permutaion(n:int,r: int) -> int:
+    return factorial(n) // factorial(n-r)
 
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-"""
-import math
+def my_combination(n:int, r:int) -> int:
+    return my_permutaion(n, r) // factorial(r)
 
 n, r = list(map(int, input().split()))
-ans = math.factorial(n) // (math.factorial(r) * math.factorial(n-r))
+ans = my_combination(n, r)
 print(ans)
