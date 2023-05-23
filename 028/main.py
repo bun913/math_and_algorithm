69d26db477f36ae76f19acd3857b64f8eceec26e
bun913 <imanau03@gmail.com>
2023-05-24 08:33:20 +0900

    feat: solve 028 dp1

--- 028/main.py
+++ 028/main.py
@@ -1,21 +1,16 @@
-"""
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
-"""
-
+N = int(input())
+H = list(map(int, input().split()))
+H.insert(0,0)
+dp = [-1 for _ in range(N+1)]
+dp[0] = 0
+for i in range(1, N+1):
+    if i == 1:
+        dp[i] = 0
+        continue
+    if i == 2:
+        dp[i] = abs(H[i]- H[i-1])
+        continue
+    c1 = dp[i-1] + abs(H[i]-H[i-1])
+    c2 = dp[i-2] + abs(H[i]-H[i-2])
+    dp[i] = min(c1, c2)
+print(dp[-1])
