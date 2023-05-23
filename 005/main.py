56f1cac17f73eff4045084d7e593789aae0650b5
bun913 <imanau03@gmail.com>
2023-05-24 06:48:02 +0900

    feat: solve 004 005 006

--- 005/main.py
+++ 005/main.py
@@ -1,24 +1,9 @@
 """
-Atcoderの問題解く用
 
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
-n = input()
-l = list(map(int, input().split()))
-print(sum(l) % 100)
+N = int(input())
+A = list(map(int, input().split()))
+ans = 0
+for a in A:
+    ans += a % 100
+print(ans % 100)
