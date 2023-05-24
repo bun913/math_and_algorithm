73a7ff8f5b73771449a5f35c28edbc389b1d91a6
bun913 <imanau03@gmail.com>
2023-05-25 06:10:45 +0900

    feat: solve dp2

--- 029/main.py
+++ 029/main.py
@@ -1,21 +1,12 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+これも動的計画法で解ける
+N段目までに至るまでのそれぞれの段数を求めていけば良い
 """
+N = int(input())
+dp = [-1 for _ in range(N+1)]
+dp[0] = 1
+dp[1] = 1
 
+for i in range(2,N+1):
+    dp[i] = dp[i-1] + dp[i-2]
+print(dp[N])
