e365a1d0fa539eb601ec4a774239df91cca28715
bun913 <imanau03@gmail.com>
2023-05-28 06:53:04 +0900

    feat: add 規則性の問題060

--- 060/main.py
+++ 060/main.py
@@ -1,32 +1,16 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+実際に規則性を考えてみる
+Nが3以下の時は先手が絶対勝つ
+Nが4の時は後手が絶対勝つ
+Nが5の時は先手が絶対勝つ
+と繰り返していくと残4の状態を先に持った方が負ける
 """
-# 規則性を見つけるとあっさり解ける類の問題
-# Nを実際に考えてみる1~3の場合先行が勝てる
-# 4の場合は絶対後衛がかつ
-# 5~7の場合は絶対に選考がかつ(4を後衛に撮らせれば良い)
-# 8の場合は絶対に後攻がかつ
-# 9~11の場合は絶対に先行がかつ(8・4を後衛にとらせればよい)
 
-n = int(input())
-ans = "First"
-if n % 4 == 0:
-    ans = "Second"
-print(ans)
+N = int(input())
+if N <= 3:
+    print('First')
+    exit()
+if N % 4 == 0:
+    print('Second')
+    exit()
+print('First')
