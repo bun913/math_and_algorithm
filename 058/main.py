c12552f80eb12d9ab4793b3158d8bf19c786c3cb
bun913 <imanau03@gmail.com>
2023-05-28 05:39:59 +0900

    feat: solve mat058

--- 058/main.py
+++ 058/main.py
@@ -1,21 +1,12 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+N, X, Y = list(map(int, input().split()))
 
+abs_dist = abs(X) + abs(Y)
+cond1 = abs_dist <= N
+cond2 = N % 2 == abs_dist % 2
+
+if cond1 and cond2:
+    print('Yes')
+    exit()
+print('No')
