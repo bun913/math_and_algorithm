75d3f5df82329a34bf8eaba413631077043ed532
bun913 <imanau03@gmail.com>
2023-05-28 17:10:54 +0900

    feat: solve 061

--- 061/main.py
+++ 061/main.py
@@ -1,35 +1,13 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+1,3,7,15という形で負けの状態がくる
 """
-# Nこの時に勝ち、負けとなる状態を考える
-
-n = int(input())
-ans = "First"
-flg = False
-
-for i in range(1,60):
-    if n == (2 ** i) - 1:
-        flg = True
-        break
-
-if flg:
-    ans = "Second"
-
-print(ans)
+N = int(input())
+# 紛らわしいので先に弾く
+a = 1
+while a <= N:
+    if a == N:
+        print("Second")
+        exit()
+    a *= 2
+    a += 1
+print("First")
