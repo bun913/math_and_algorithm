b85b2cbd0ad8bdcb30cca19f36928f7f7c364417
bun913 <imanau03@gmail.com>
2023-05-28 17:11:01 +0900

    feat: solve 062

--- 062/main.py
+++ 062/main.py
@@ -1,80 +1,38 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input()
-#int型で受け取るとき
-s = int(input())
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+テレポーターは絶対にどっかのタイミングでループするので
+剰余の考えを使えばNの計算でいけそう
+初めて訪れた時の手数をFirstという配列で記録
+2回目に訪れた時の手数をSecondという配列で記録
 """
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
-n, k = list(map(int, input().split()))
-l = list(map(int, input().split()))
+# 初期化
+firsts = [-1 for _ in range(N+1)]
+seconds = [-1 for _ in range(N+1)]
+dist = {}
+for i, a in enumerate(A):
+    dist[i+1] = a
 
-# すでに１度訪れた街かを記憶する
-town_mem = set()
-
-# こいつの中にバグがある
-def find_base_town(l:list, town_mem: set,town_num: int):
-    # ベースケース
-    if town_num in town_mem:
-        return town_num
-    town_mem.add(town_num)
-    return find_base_town(l, town_mem,l[town_num-1])
-
-# どこの街を起点に何回でその街に戻ってくるのかを調べる
-base_town = find_base_town(l, town_mem, 1)
-town_mem = set()
-# 最初に起点の街に辿り着くまでに必要な回数
-flg = True
-cur = 1
-ini_c = 0
-while flg:
-    # 終了条件
-    if cur == base_town:
-        flg = False
-        break
-    ini_c += 1
-    cur = l[cur-1]
-
-# ループのための回数
-# 純粋にベースとなる街から２回目にその街に戻るまでのループ回数
-base_count = 0
-cur = 1
-loop_count = 0
-while base_count <= 1:
-    if base_count == 1:
-        loop_count += 1
-    if cur == base_town:
-        base_count += 1
-    cur = l[cur-1]
-
-# k回は最小何回テレポートしたのと同じなのか計算
-# ループの回数で割理続ける
-def calc(n:int, loop_count):
-    # ベースケース
-    mod = n % loop_count
-    if mod >= loop_count:
-        return calc(mod, loop_count)
-    else:
-        return mod
-
-terepo_count = calc(k-ini_c, loop_count)
-
-ans = 0
-next_town = 1
-for i in range(1, ini_c+terepo_count+1):
-    next_town = l[next_town-1]
-
-print(next_town)
+# 町1からスタート
+cnt = 0
+cur_town = 1
+while True:
+    # firstsの更新
+    if firsts[cur_town] == -1:
+        firsts[cur_town] = cnt
+    # secondsの更新
+    elif seconds[cur_town] == -1:
+        seconds[cur_town] = cnt
+    # ループに至る前に終了する場合
+    if cnt == K:
+        print(cur_town)
+        exit()
+    # ループに入って戻ってきている場合
+    if firsts[cur_town] != -1 and seconds[cur_town] != -1:
+        L = seconds[cur_town] - firsts[cur_town]
+        if (K - firsts[cur_town]) % L == 0:
+            print(cur_town)
+            exit()
+    cnt += 1
+    cur_town = dist[cur_town]
