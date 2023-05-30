8dbd660b17124c93dbee1732d8f592487fa85552
bun913 <imanau03@gmail.com>
2023-05-31 06:09:08 +0900

    feat: solve 058

--- 059/main.py
+++ 059/main.py
@@ -1,44 +1,18 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+2**Nの一の位を求める
+あくまでかけ続けるのは2で固定
+つまり1の位はループする
+2,4,8,6のループとなる
 """
+N = int(input())
 
-# まず以下のように考える
-# 普通に計算して1のくらいの桁を導出すれば良いのでは
-# でもそこでNが10**12までとりうるということに着目
-# 1秒以内で計算するのは厳しいと確認
-# そこで具体的に考えてみる
-# n = 1 2, n=2 4, n=3 8, n=4 6
-# n=5, 2, n=6 4, n=8 8, n=9 6
-# これを繰り返すことがわかる
-
-n = int(input())
-
-ans = 0
-
-if n % 4 == 0:
-    ans = 6
-elif n % 4 == 3:
-    ans = 8
-elif n % 4 == 2:
-    ans = 4
-elif n % 4 == 1:
-    ans = 2
-
-print(ans)
+if N %4 == 1:
+    print(2)
+    exit()
+elif N % 4 == 2:
+    print(4)
+    exit()
+elif N % 4 == 3:
+    print(8)
+    exit()
+print(6)
--- 063/main.py
+++ 063/main.py
@@ -1,25 +1,9 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+N*Nのますを考える
+マス目を市松模様で塗り分けた時に白と黒のマスの目の数が同じにならないと無理
 """
-n = int(input())
-ans = "No"
-if n % 2 == 0:
-    ans = "Yes"
-print(ans)
+N = int(input())
+if N % 2 == 0:
+    print("Yes")
+    exit()
+print("No")
