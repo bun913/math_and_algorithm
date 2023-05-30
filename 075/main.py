999a40f85487a1730d7b5b915f1906648d9ba3de
bun913 <imanau03@gmail.com>
2023-05-31 06:47:01 +0900

    feat: add 075 足し算ピラミッドの一般化

--- 075/main.py
+++ 075/main.py
@@ -1,21 +1,40 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+足し算ピラミッドの問題を一般化したもの
+Nが200,000と大きいので愚直に計算すると間に合わない
+左からi番目の数字をa_iとして、それが足される回数を考える
+a_iが足される回数はn-1Ciとなる
 """
 
+class ModuloComb:
+  def __init__(self, mod, num_factorial):
+      self.mod = mod
+      self.factorial = [ None ] * (num_factorial + 1) # factorial[i] = i! % mod
+      self.factorial[0] = 1
+      for i in range(1, num_factorial + 1):
+          self.factorial[i] = self.factorial[i - 1] * i % self.mod
+  
+  def comb(self, n, r):
+      return self.moddiv(self.factorial[n], self.factorial[r] * self.factorial[n - r] % self.mod)
+  
+  def moddiv(self, a, b): # (a / b) % mod
+      return (a * self.modpow(b, self.mod - 2)) % self.mod
+  
+  def modpow(self, a, b):
+      p = a
+      answer = 1
+      for i in range(30):
+          if (b & (1 << i)) != 0:
+              answer = (answer * p) % self.mod
+          p = (p * p) % self.mod
+      return answer
+
+N = int(input())
+MOD = 10**9 + 7
+A = list(map(int, input().split()))
+
+ans = 0
+combo_module = ModuloComb(MOD, N - 1)
+for i,a in enumerate(A):
+    cnt = combo_module.comb(N-1,i)
+    ans += a * cnt % MOD
+print(ans % MOD)
