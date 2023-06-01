a8c87b24598104e41e41bae499ccfda2f0f8e171
bun913 <imanau03@gmail.com>
2023-06-01 09:57:44 +0900

    feat: solve 076 足される数を考える問題のちょっと応用

--- 076/main.py
+++ 076/main.py
@@ -1,21 +1,15 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+足される数を考える問題
+絶対値というのがポイントで配列を大きい順に並び替えても結果は同じ
+単純に書く項目の足される数を考えて一般化した式を作れば良いだけ
 """
 
+N = int(input())
+A = list(map(int, input().split()))
+ad = sorted(A, reverse=True)
+ans = 0
+
+for i in range(N):
+    cnt = (N-2*i-1) * ad[i]
+    ans += cnt
+print(ans)
