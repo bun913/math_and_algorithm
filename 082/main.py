6e529bd7db89b959c5130251d7d49082055b4d88
bun913 <imanau03@gmail.com>
2023-06-04 06:39:41 +0900

    feat: 区間スケジューリング問題の082を解答

--- 082/main.py
+++ 082/main.py
@@ -1,21 +1,21 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+この場合映画に満足度などはなく単純に数をこなせば良い
+であれば、貪欲法で今見れる映画で時間がいちばん早く終わるものを選び続ければOK
+これをOlogNで実装するには、区間スケジューリング問題というものを利用する
+あらけじめ終了時刻の早い時間順にソートしておく
 """
+from operator import itemgetter
 
+# 終了時刻が小さい順にidを並び替える
+N = int(input())
+LR = [list(map(int, input().split())) for _ in range(N)]
+lrd = sorted(LR, key=itemgetter(1))
+
+# 終了時刻が早い順に見ていく
+ans = 0
+last = 0 # 前回の映画の終了時刻
+for st,en in lrd:
+    if last <= st:
+        ans += 1
+        last = en
+print(ans)
