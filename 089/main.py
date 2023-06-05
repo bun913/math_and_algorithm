58c7cfa51511531f01739ac953cfece4fb0815a2
bun913 <imanau03@gmail.com>
2023-06-05 09:19:20 +0900

    feat: 対数の性質と大きい数の冪乗

--- 089/main.py
+++ 089/main.py
@@ -1,21 +1,18 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+指数法則と式の変形で整数の世界に持ち込む
 """
-
+a, b, c = list(map(int, input().split()))
+# cが1の場合は、aとbの大小関係は問わない
+if c == 1:
+    print("No")
+    exit()
+v = 1
+# 普通に c ** bを計算すると、オーバーフローする可能性がある
+# 随時計算していってaを越えたら終了
+for i in range(1,b+1):
+    # a < (v * c)を言い換え
+    if a // c < v:
+        print("Yes")
+        exit()
+    v *= c
+print("No")
