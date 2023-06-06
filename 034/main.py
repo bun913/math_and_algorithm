f22ce2f15cc40bd34aaaf1c234499cd8ef228059
bun913 <imanau03@gmail.com>
2023-06-07 06:07:58 +0900

    feat: solve 034 点同志の距離

--- 034/main.py
+++ 034/main.py
@@ -1,21 +1,15 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+2次元平面上のN個の点で最も近い2つの点の距離を求める
+Nが2000なのでコンビネーションで求めても大丈夫そう
 """
+N = int(input())
+points = [list(map(int, input().split())) for _ in range(N)]
 
+ans = float('inf')
+for i in range(N):
+    for j in range(i+1, N):
+        x1, y1 = points[i]
+        x2, y2 = points[j]
+        dis = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+        ans = min(ans, dis)
+print(ans)
