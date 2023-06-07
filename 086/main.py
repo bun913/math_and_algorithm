b4ccb805a43130059bb249ba6d28f54f26b37d4e
bun913 <imanau03@gmail.com>
2023-06-08 08:20:06 +0900

    feat: add 086

--- 086/main.py
+++ 086/main.py
@@ -1,21 +1,25 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+N文字の`(`か`)`からなる文字列Sが与えられる。
+Sが正しいかっこ文字列か判定する。
+これもいきなり正解を導き出そうとせずに、動的計画法みたいにn文字目までが正しいかっこ列かを考える
+- n文字目までに登場する(の数が)の数以上であること
+- 最終的に(の数と)の数が同じであること
+と求められる
 """
+N = int(input())
+S = input()
+l_cnt = 0
+r_cnt = 0
 
+for s in S:
+    if s == "(":
+        l_cnt += 1
+    else:
+        r_cnt += 1
+    if l_cnt < r_cnt:
+        print("No")
+        exit()
+if l_cnt == r_cnt:
+    print("Yes")
+else:
+    print("No")
