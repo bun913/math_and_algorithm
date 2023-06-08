3af1feea95ea121df41b50323f4e51ab52a19e55
bun913 <imanau03@gmail.com>
2023-06-09 06:25:14 +0900

    feat: accumrate 038

--- 038/main.py
+++ 038/main.py
@@ -1,21 +1,13 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
+from itertools import accumulate
 
+N, Q = list(map(int, input().split()))
+A = list(map(int, input().split()))
+acc = list(accumulate(A))
+acc.insert(0,0)
+
+for _ in range(Q):
+    l,r = list(map(int, input().split()))
+    ans = acc[r] - acc[l-1]
+    print(ans)
