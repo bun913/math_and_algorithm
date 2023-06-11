cd57241776c0dd19a8114bed025e70151604f48e
bun913 <imanau03@gmail.com>
2023-06-11 11:42:44 +0900

    feat: 040 累積和

--- 040/main.py
+++ 040/main.py
@@ -1,21 +1,20 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+これも典型的な累積和の問題
 """
+from itertools import accumulate
 
+N = int(input())
+A = list(map(int, input().split()))
+A.insert(0,0)
+acc = list(accumulate(A))
+M = int(input())
+B = [int(input()) for _ in range(M)]
+
+# B[i]をl,B[i+1]をrとして累積和から距離の合計を求める
+# 面倒なのでl = min(l,r)としておく
+ans = 0
+for i in range(M-1):
+    l = min(B[i], B[i+1])
+    r = max(B[i], B[i+1])
+    ans += acc[r-1] - acc[l-1]
+print(ans)
