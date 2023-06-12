c03ae75578a8ca98a9f6a88a1ba70fdf0b289bef
bun913 <imanau03@gmail.com>
2023-06-12 09:45:31 +0900

    feat: 041 階差の問題

--- 041/main.py
+++ 041/main.py
@@ -1,21 +1,18 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+一見何のこっちゃわからんけど、階差の問題だ
+答えが[0,1,...,T-1]に何人の従業員がいるか答える問題
+なので階差で答えを保持しておけば良いだけだわ
 """
+from itertools import accumulate
 
+T = int(input())
+N = int(input())
+dif = [0 for _ in range(T+1)]
+for _ in range(N):
+    l, r = map(int, input().split())
+    dif[l] += 1
+    dif[r] -= 1
+# 階差の累積和が答え
+ans = list(accumulate(dif))
+for a in range(T):
+    print(ans[a])
