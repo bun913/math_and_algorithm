440a7f2866b4f200c286fff3a103f1d73534c9ea
bun913 <imanau03@gmail.com>
2023-06-14 06:52:28 +0900

    feat: 042 エラストテネスのふるいの実装的な

--- 042/main.py
+++ 042/main.py
@@ -1,21 +1,13 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+エラストテネスのふるいのようにf(i)を計算する
 """
-
+N = int(input())
+f_list = [0 for _ in range(N+1)]
+for i in range(1, N+1):
+    # iの倍数
+    for j in range(i, N+1,i):
+        f_list[j] += 1
+ans = 0
+for i in range(1, N+1):
+    ans += i*f_list[i] 
+print(ans)
