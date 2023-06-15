dba77a109d9184ec7db9e6c82a1a439ff226e823
bun913 <imanau03@gmail.com>
2023-06-16 06:22:22 +0900

    feat: solve 084

--- 084/main.py
+++ 084/main.py
@@ -1,21 +1,16 @@
 """
-Atcoderの問題解く用
+一見普通に計算すれば良いのだけどa,b,cがとても大きく、ルートを取るとオーバーフローしてしまう
+なのでsqrt(a) + sqrt(b) < sqrt(c)という条件を満たすように変形する
 
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
 """
-
+a, b, c = list(map(int, input().split()))
+left = 4 * a * b
+right = (c - a - b) ** 2
+# c-a-bがマイナスになる時は絶対に条件を満たさない
+if c - a- b < 0:
+    print("No")
+    exit()
+if left < right:
+    print("Yes")
+    exit()
+print("No")
