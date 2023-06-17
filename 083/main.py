bc22401a14862aebd5bbf93482cc16fc6ad504b4
bun913 <imanau03@gmail.com>
2023-06-17 09:05:43 +0900

    feat: solve 083

--- 083/main.py
+++ 083/main.py
@@ -1,21 +1,20 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+今回の条件をちゃんと整理すると
+A1,A2,...,ANは全て異なる。さらに全部0以上の整数（マイナスはない）
+B1,B2,...,BNは全て異なる。さらに全部0以上の整数（マイナスはない）
+てことはAiからBiを選ぶにあたって個々の最適はバッティングしない
+なのでA1...から順に最適な経路をとっていけば良いだけ
+もっというとAとBをソートしてそれぞれの値を取るだけ
 """
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+ad = sorted(A)
+bd = sorted(B)
 
+ans = 0
+for i in range(N):
+    a = ad[i]
+    b = bd[i]
+    ans += abs(a-b)
+print(ans)
