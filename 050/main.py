0b6a52281e0e3ad718e3909b467764dee4df7fc0
bun913 <imanau03@gmail.com>
2023-06-18 16:34:43 +0900

    feat: 050 繰り返し二乗法

--- 050/main.py
+++ 050/main.py
@@ -1,21 +1,26 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+a**bを1000000007で割った余りを求めよ
+繰り返し二乗法で実装する
+指数を2の累乗の積に分解する
 """
 
+def mod_pow(a,n,mod):
+    """
+    繰り返し二乗法で実装している
+    指数を2進数に変換している
+    変換した結果桁が1になっているところの積を求めている
+    """
+    e = n
+    base = a
+    result = 1
+    while e > 0:
+        if e % 2 == 1:
+            result *= base
+            result %= mod
+        base *= base
+        base %= mod
+        e //= 2
+    return result
+
+a, b = map(int, input().split())
+print(mod_pow(a, b, 1000000007))
