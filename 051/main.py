b22de14977949722662d3621ac743e3ff25d00c0
bun913 <imanau03@gmail.com>
2023-06-18 16:34:26 +0900

    feat: solve 逆元とあまりの性質

--- 051/main.py
+++ 051/main.py
@@ -1,21 +1,27 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+数学的にいうと
+(X + Y )! // X! * Y!が答えとなるはず
+ただ、普通に計算するととんでもなく大きい数となってしまう
+掛け算の場合は途中であまりをとっても良い性質であることを利用して考える
 """
+X, Y = map(int, input().split())
+mod = 1000000007
 
+# xの階乗で随時あまりを取る
+x_fact = [1]
+for i in range(1, X+1):
+    nex = x_fact[-1] * i % mod
+    x_fact.append(nex)
+# yの階乗で随時あまりを取る
+y_fact = [1]
+for i in range(1, Y+1):
+    nex = y_fact[-1] * i % mod
+    y_fact.append(nex)
+# x+yの階乗で随時あまりを取る
+xy_fact = [1]
+for i in range(1, X+Y+1):
+    nex = xy_fact[-1] * i % mod
+    xy_fact.append(nex)
+# 割り算なので逆元を求める
+ans = xy_fact[-1] * pow(x_fact[-1], mod-2, mod) * pow(y_fact[-1], mod-2, mod) % mod
+print(ans)
