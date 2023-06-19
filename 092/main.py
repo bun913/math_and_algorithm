7e2515308c29a9b57f42d79dd8383890f5e3b9a8
bun913 <imanau03@gmail.com>
2023-06-19 21:15:46 +0900

    feat: solve 092

--- 092/main.py
+++ 092/main.py
@@ -1,21 +1,14 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+O(N)だと無理だけどO(logN)なら間に合うので、
+a * bとなる整数を探索するけど、平方根まで探索すれば十分
 """
+N = int(input())
 
+ans = float('inf')
+for a in range(1, int(N**0.5)+1):
+   if N % a != 0:
+       continue 
+   b = N // a
+   circ = 2 * a + 2 * b
+   ans = min(ans, circ)
+print(ans)
