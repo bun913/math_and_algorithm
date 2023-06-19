880fad9300eb154142e832ab9d050ca55b38313c
bun913 <imanau03@gmail.com>
2023-06-19 21:10:31 +0900

    feat: solve 091

--- 091/main.py
+++ 091/main.py
@@ -1,21 +1,12 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+普通に全探索で間に合う
 """
+N, X = map(int, input().split())
+ans = 0
 
+for a in range(1, N-1):
+    for b in range(a+1, N):
+        for c in range(b+1, N+1):
+            if a + b + c == X:
+                ans += 1
+print(ans)
