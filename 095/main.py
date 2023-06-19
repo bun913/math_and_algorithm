3f65c017c965c83f3e4504f3d521259c73a4e73a
bun913 <imanau03@gmail.com>
2023-06-20 06:03:52 +0900

    feat: 累積和の問題

--- 095/main.py
+++ 095/main.py
@@ -1,21 +1,24 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+なんてわかりやすい累積和の問題なんだ
 """
+from itertools import accumulate
 
+N = int(input())
+C1 = [0]
+C2 = [0]
+for _ in range(N):
+    c, p = map(int, input().split())
+    if c == 1:
+        C1.append(p)
+        C2.append(0)
+    else:
+        C1.append(0)
+        C2.append(p)
+cum1 = list(accumulate(C1))
+cum2 = list(accumulate(C2))
+Q = int(input())
+for _ in range(Q):
+    l , r = map(int, input().split())
+    ans1 = cum1[r] - cum1[l-1]
+    ans2 = cum2[r] - cum2[l-1]
+    print(ans1, ans2, sep=" ")
