6f45c06197b6fcd08523648edfa2263ae190e5aa
bun913 <imanau03@gmail.com>
2023-06-21 06:55:29 +0900

    chore: 098,099が解けなかった

--- 098/main.py
+++ 098/main.py
@@ -1,21 +1,6 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+点A,Bが多角形に含まれるかの判定
+点A、Bから左に向かって半直線を引き、多角形の辺と交わった回数を数える
+奇数回なら含まれる、偶数回なら含まれない
 """
 
--- 099/main.py
+++ 099/main.py
@@ -1,21 +1,29 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+グラフだけどNが10**6とかなりでかい
 """
-
+from collections import deque
+N = int(input())
+# グラフの作成
+G = [[] for _ in range(N)]
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    a -= 1; b -= 1
+    G[a].append(b)
+    G[b].append(a)
+# 距離を格納する
+dist = [-1] * N
+dist[0] = 0
+q = deque([0])
+# 深さ優先探索をする
+ans = 0
+while q:
+    pos = q.popleft()
+    for nex in G[pos]:
+        # 訪問済みである場合はスキップ
+        if dist[nex] != -1:
+            continue
+        q.append(nex)
+        dist[nex] = dist[pos] + 1
+        ans += dist[pos] + 1
+# 答えを求める
+print(ans)
