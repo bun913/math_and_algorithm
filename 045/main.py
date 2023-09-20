684feca5eb007ad15d52ff51230d66dd6f28e212
bun913 <nagasima.romance+@gmail.com>
2023-09-20 13:52:29 +0900

    feat: 045 047

--- 045/main.py
+++ 045/main.py
@@ -1,21 +1,29 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+グラフを作るのは問題ない
+それぞれの頂点の中で自分自身より頂点番号が小さい隣接頂点が1つだけある頂点の数
 """
+N, M = map(int, input().split())
 
+# グラフを作成
+G = [[] for _ in range(N)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    G[a-1].append(b-1)
+    G[b-1].append(a-1)
+# 判定
+ans = 0
+for i,g in enumerate(G):
+    # 隣接頂点なし
+    if len(g) == 0:
+        continue
+    sg = sorted(g)
+    min_val = sg[0]
+    # 要素が一つしかない
+    if len(g) == 1:
+        if min_val < i:
+            ans += 1
+        continue
+    second_min = sg[1]
+    if min_val < i and second_min > i:
+        ans += 1
+print(ans)
--- 047/main.py
+++ 047/main.py
@@ -1,21 +1,35 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+二部グラフかどうかを判定する
+実際に幅優先探索で色を塗っていく
 """
+from collections import deque
 
+# 入力を受け取る
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]  # グラフを表現する隣接リスト
+for i in range(M):
+    a, b = map(int, input().split())
+    G[a-1].append(b-1)
+    G[b-1].append(a-1)
+color = [0] * N
+# colorは0:未訪問, 1:グループA, 2:グループB
+for v in range(N):
+    # 訪問済みはスキップ
+    if color[v] != 0:
+        continue
+    # 幅優先探索を行う
+    q = deque([v])
+    while q:
+        qv = q.popleft()
+        for nex in G[qv]:
+            # 未訪問の場合は色を塗る
+            if color[nex] == 0:
+                next_color = 2 - color[qv]
+                color[nex] = next_color
+                q.append(nex)
+                continue
+            # 訪問済みの場合は色が同じであれば即終了
+            if color[qv] == color[nex]:
+                print("No")
+                exit()
+print("Yes")
