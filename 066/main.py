069f282a90c58cfbc65cefe6bb0243ad6e91d184
bun913 <nagasima.romance+@gmail.com>
2023-09-30 07:21:35 +0900

    feat: solve 066 070

--- 066/main.py
+++ 066/main.py
@@ -1,21 +1,25 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+そもそもの条件がorで繋がっていて候補数が多すぎる
+と考えれば
+赤を固定して
+青と赤の差の絶対値がKより小さい
+白と赤の差の絶対値がKより小さい
+という条件を全て満たすものを数える
 """
+N,K = list(map(int, input().split()))
 
+ce = 0
+# 赤の候補を固定してみる
+for red in range(1, N+1):
+    # 青は赤との差の絶対値がKより小さくなるように全探索
+    for blue in range(red-K+1, red+K):
+        if blue < 1 or blue > N:
+            continue
+        # 白は赤との差の絶対値がKより小さくなるように全探索
+        for white in range(red-K+1, red+K):
+            if white < 1 or white > N:
+                continue
+            if abs(blue-white) < K:
+                ce += 1
+ans = N * N * N - ce
+print(ans)
--- 070/main.py
+++ 070/main.py
@@ -1,21 +1,31 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+点とぶつからない長方形は満たさない
+なぜなら点とぶつかることで、囲む点の数を変えずに面積を小さくすることができるから
+Kはたかだが50なので、Kの点の最小値を取ることで、全探索できる
 """
+from itertools import combinations
 
+# 4点の座標の中にK点以上含まれるかどうかを判定する
+def check_numpoints(N,X, Y, lx, rx, ly, ry):
+    cnt = 0
+    for i in range(N):
+        if lx <= X[i] <= rx and ly <= Y[i] <= ry:
+            cnt += 1
+    return cnt >= K
+
+N, K = map(int, input().split())
+xy = [list(map(int, input().split())) for _ in range(N)]
+X = [xy[i][0] for i in range(N)]
+Y = [xy[i][1] for i in range(N)]
+
+ans = float("inf")
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            for l in range(N):
+                cl,cr,dl,dr = X[i], X[j], Y[k], Y[l]
+                # 長方形を作るためにcl < cr, dl < drである必要がある
+                if cl < cr or dl < dr:
+                    if check_numpoints(N,X,Y,cl,cr,dl,dr):
+                        ans = min(ans, (cr-cl)*(dr-dl))
+print(ans)
