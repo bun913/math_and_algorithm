3cc037c55bd86be06b0dda7f25745f7991b11ba4
bun913 <nagasima.romance+@gmail.com>
2023-10-04 06:35:07 +0900

    feat: 073

--- 073/main.py
+++ 073/main.py
@@ -1,21 +1,21 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+N個の整数A1,A2,...,ANが与えらる。
+1個以上の整数を選ぶ組み合わせは2**N-1通りある。
+1個以上の整数を選んだ時の、「選んだ整数の最大値」を全て足した値
+を1000000007で割ったあまりを求める。
 """
+N = int(input())
+A = list(map(int, input().split()))
+MOD = 1000000007
+power = [0 for _ in range(N)]
+power[0] = 1
+# べき乗の計算はこれがいちばん早いみたい
+for i in range(1, N):
+    power[i] = (power[i-1] * 2) % MOD
 
+ans = 0
+for i,a in enumerate(A):
+    cnt = a * power[i]
+    ans += cnt
+    ans %= MOD
+print(ans)
