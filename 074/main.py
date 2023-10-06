5545817535058eab5ecfb23c93f78fedbf50a9a2
bun913 <nagasima.romance+@gmail.com>
2023-10-07 08:30:09 +0900

    feat: 074

--- 074/main.py
+++ 074/main.py
@@ -1,21 +1,13 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+足される回数と引かれる回数を考える問題
+これは考え方を知ってないと絶対に解けない気がする
+逆に考え方を知っていれば、実装自体は楽
 """
+from functools import reduce
 
+N = int(input())
+A = list(map(int, input().split()))
+ans = 0
+for i, a in enumerate(A):
+    ans += a * (2 * i - N + 1)
+print(ans)
