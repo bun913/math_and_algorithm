83ac23835499a017a8bff4ab833fd3398f377912
bun913 <nagasima.romance+@gmail.com>
2023-10-09 06:33:28 +0900

    feat: 080

--- 080/main.py
+++ 080/main.py
@@ -1,21 +1,16 @@
 """
-Atcoderの問題解く用
-
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+貪欲法・・・その時の最善手を選びつづけること
+リバーシで言えば、その時に取れる最大の駒を取り続けること
+必ずしもし最適とは限らない
+aを利用する10000円札の数、bを5000円札の数、cを1000円札の数とする
+nは1000の倍数なのであまりを考えずに
 """
+N = int(input())
 
+a = N // 10000
+bn = N % 10000
+b = bn // 5000
+cn = bn % 5000
+c = cn // 1000
+
+print(a+b+c)
