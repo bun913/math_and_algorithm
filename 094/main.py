5badadfc1a270e78740e8d084c77a9e4a0700485
bun913 <nagasima.romance+@gmail.com>
2023-10-12 15:43:03 +0900

    feat: 094

--- 094/main.py
+++ 094/main.py
@@ -1,21 +1,23 @@
 """
-Atcoderの問題解く用
+Nこの整数からなる数列Aがあるが、値がわからない
+BはN-1個の数列で値がわかっている
+max(Ai, Ai+1) <= Biを満たしている
+Aの合計としてありうる最大の値を求めよ
 
-1行1列データ
-
-#str型で受け取るとき
-s = input() 
-#int型で受け取るとき
-s = int(input()) 
-#float型　(小数)で受け取るとき
-s = float(input())
-
-(1,N)行列データ
-s = input().split()
-# listで整数で受け取る
-l = list(map(int, input().split()))
-
-その他
-https://qiita.com/jamjamjam/items/e066b8c7bc85487c0785
+そのためAi,Ai+1は両方Bi以下
+さらにAi+1,Ai+2は両方Bi+1以下
+Ai+1の最大値はmin(Bi,Bi+1)である
 """
+N = int(input())
+B = list(map(int, input().split()))
+A = [0] * N
 
+for i in range(N):
+    if i ==0:
+        A[i] = B[i]
+        continue
+    if i == N-1:
+        A[i] = B[i-1]
+        continue
+    A[i] = min(B[i-1], B[i])
+print(sum(A))
